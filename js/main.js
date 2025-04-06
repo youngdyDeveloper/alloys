dfdb17860ecb9190438df17d9a90416b58b0b145
youngdyDeveloper <ndossdavid8@gmail.com>
2025-04-06 21:52:56 +0300

    semi

--- js/main.js
+++ js/main.js
@@ -8,60 +8,60 @@ $(document).ready(function() {
   // ========================================================================= //
 
 
-  $(document).on("scroll", onScroll);
+  // $(document).on("scroll", onScroll);
 
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
+  // $('a[href^="#"]').on('click', function(e) {
+  //   e.preventDefault();
+  //   $(document).off("scroll");
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
-    });
+  //   $('a').each(function() {
+  //     $(this).removeClass('active');
+  //     if ($(window).width() < 768) {
+  //       $('.nav-menu').slideUp();
+  //     }
+  //   });
 
-    $(this).addClass('active');
+  //   $(this).addClass('active');
 
-    var target = this.hash,
-        menu = target;
+  //   var target = this.hash,
+  //       menu = target;
 
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
-  });
-
-
-  function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
-  }
-
-  // ========================================================================= //
-  //  //NAVBAR SHOW - HIDE
-  // ========================================================================= //
+  //   target = $(target);
+  //   $('html, body').stop().animate({
+  //     'scrollTop': target.offset().top - 80
+  //   }, 500, 'swing', function() {
+  //     window.location.hash = target.selector;
+  //     $(document).on("scroll", onScroll);
+  //   });
+  // });
 
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
-  });
+  // function onScroll(event) {
+  //   if ($('.home').length) {
+  //     var scrollPos = $(document).scrollTop();
+  //     $('nav ul li a').each(function() {
+  //       var currLink = $(this);
+  //       var refElement = $(currLink.attr("href"));
+  //     });
+  //   }
+  // }
+
+  // // ========================================================================= //
+  // //  //NAVBAR SHOW - HIDE
+  // // ========================================================================= //
+
+
+  // $(window).scroll(function() {
+  //   var scroll = $(window).scrollTop();
+  //   if (scroll > 200 ) {
+  //     $("#main-nav, #main-nav-subpage").slideDown(700);
+  //     $("#main-nav-subpage").removeClass('subpage-nav');
+  //   } else {
+  //     $("#main-nav").slideUp(700);
+  //     $("#main-nav-subpage").hide();
+  //     $("#main-nav-subpage").addClass('subpage-nav');
+  //   }
+  // });
 
   // ========================================================================= //
   //  // RESPONSIVE MENU
